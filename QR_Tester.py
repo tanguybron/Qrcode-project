dea04768afdafe6e264375674d8fd5f70bd88e67
tanguybron <63880113+tanguybron@users.noreply.github.com>
2020-12-09 18:07:50 +0100

    ⚡️ last maj

--- QR_Tester.py
+++ QR_Tester.py
@@ -3,7 +3,7 @@ from main import make
 # Crée un objet Qr Code
 # Ajoute les données à ce Qr Code
 # Le Transforme en image
-qr = make('ok')
+qr = make('perso.esiee.fr/~vigierj')
 
 qr.save('qr_img.png')
 print("qr code créé.")
